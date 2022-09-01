d9b08c865e05de1a42350f9deef5b2b2473319e2
Aaron Will Djaba <papiliocurtis@gmail.com>
2022-09-01 17:11:53 +0000

    kurtiz :alembic:

--- functions.py
+++ functions.py
@@ -1,14 +1,26 @@
 # What are functions
 # Structure and components of a function in python
+def name_of_function():
+    # block of code
+    something = "Hello World"
+    print(something)
+
+
+name_of_function()
+
 
 def add():
     sum = 2+4
     print(sum)
 
 
+add()
+
 # let's try another example
+
+
 def multiply():
     product = 5*9
     print(product)
 
-
+multiply()
---
+++ myFunctions.py
@@ -0,0 +1,11 @@
+def add():
+    x = 3+7+8
+    print(x)
+
+add()
+
+def anotherFunc():
+    d = 2+7-9*3-0.4
+    print(d)
+
+anotherFunc()
